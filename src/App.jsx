32f8ae94b2c812c51ec0a664585787aefbe39579
“Peehal” <sinhapeehal@gmail.com>
2025-08-27 00:38:09 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="bg-blue-500 text-white p-4 text-3xl">
+      Hello, Tailwind!
+      </div>
+      <h1>hello World</h1>
+     
+    </>
+  )
+}
+
+
+
+export default App
