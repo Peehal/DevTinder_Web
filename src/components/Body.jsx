57388f45c026be1372f97e6ef78e21d337a3dff4
“Peehal” <sinhapeehal@gmail.com>
2025-08-29 16:05:34 +0530

    Redirect to login (!auth)

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,8 +1,42 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import NavBar from "./navBar";
 import Footer from "./Footer";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { useEffect } from "react";
 
 const Body = () => {
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const userData = useSelector ((store) => store.user);
+
+  const fetchUser = async() =>{
+    if(userData) return ; 
+    try {
+      const res = await axios.get(BASE_URL + "/profile/view", {
+      withCredentials:true,
+      headers: {
+        "Cache-Control": "no-cache",
+        "Pragma": "no-cache",
+        "Expires": "0",
+      },
+    });
+    dispatch(addUser(res.data));
+  }catch(error){
+    if(error.status === 401){
+      navigate("/login");
+    }
+      console.error(error);  
+    }
+  };
+
+  useEffect(() =>{
+      fetchUser();  
+  }, []);
+
   return <div>
     <NavBar/>
     <Outlet /> 
