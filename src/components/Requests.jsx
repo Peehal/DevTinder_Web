1dd61d53f3dca248d786eda76c70dcb4fe7cda81
“Peehal” <sinhapeehal@gmail.com>
2025-08-30 17:12:41 +0530

    Feat: ConnectionRequests

--- src/components/Requests.jsx
+++ src/components/Requests.jsx
@@ -1,44 +1,43 @@
 import axios from "axios";
 import { BASE_URL } from "../utils/constants";
-import { useEffect } from "react";
-import { addConnections } from "../utils/connectionSlice";
 import { useDispatch, useSelector } from "react-redux";
+import { addRequests } from "../utils/requestSlice";
+import { useEffect } from "react";
 
-const Connections = () => {
-  const connections = useSelector((store) => store.connections);
+const Requests = () => {
+  const requests = useSelector((store) => store.requests);
   const dispatch = useDispatch();
-  const fetchConnections = async () => {
+  const fetchRequests = async () => {
     try {
-      const res = await axios.get(BASE_URL + "/user/connections", {
+      const res = await axios.get(BASE_URL + "/user/request/received", {
         withCredentials: true,
       });
-      dispatch(addConnections(res.data.data));
+      dispatch(addRequests(res.data.data));
     } catch (error) {
       console.error(error.message);
     }
   };
 
   useEffect(() => {
-    fetchConnections();
+    fetchRequests();
   }, []);
 
-  if (!connections) return;
+  if (!requests) return;
 
-  if (connections.length === 0) return <h1>No Connections Found</h1>;
+  if (requests.length === 0) return <h1>No Requests Found</h1>;
 
   return (
-    <div className=" text-center my-10">
-      <h1 className="text-bold text-white text-3xl">Connections</h1>
+    <div  className=" text-center my-10">
+      <h1 className="text-bold text-white text-3xl">Connections Requests</h1>
 
-      {connections.map((connection) => {
+      {requests.map((request) => {
         const { _id, firstName, lastName, photoUrl, gender, age, about } =
-          connection;
+          request.fromUserId;
 
         return (
           <div
-            key={_id}
-            className="flex m-4 p-4 rounded-lg bg-base-300 w-1/2 mx-auto "
-          >
+          key={_id}
+          className="flex justify-between items-center m-4 p-4 rounded-lg bg-base-300 w-1/2 mx-auto ">
             <div>
               <img
                 src={photoUrl}
@@ -54,11 +53,15 @@ const Connections = () => {
               {age && gender && <p>{age + ", " + gender}</p>}
               <p>{about}</p>
             </div>
-            
+            <div>
+              <button className="btn btn-primary mx-3">Reject</button>
+              <button className="btn btn-secondary mx-3">Accept</button>
+            </div>
           </div>
         );
       })}
     </div>
   );
 };
-export default Connections;
+
+export default Requests;
---
+++ src/utils/requestSlice.jsx
@@ -0,0 +1,12 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const requestslice = createSlice({
+    name:"requests",
+    initialState: null,
+    reducers:{
+        addRequests :(state, action ) => action.payload,
+    },
+}); 
+
+export const { addRequests } = requestslice.actions;
+export default requestslice.reducer;
